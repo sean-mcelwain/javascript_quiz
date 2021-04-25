f1358576f1650fc6415e7b31601962e81979b270
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-04-25 18:24:58 -0500

    function with timer

--- assets/script.js
+++ assets/script.js
@@ -23,14 +23,15 @@ var lineBreak01 = $("<div>").addClass("lineBreak");
 var sideBox = $("<div>").addClass("sideBox");
 var timeBox = $("<div>").addClass("timeBox");
 var timer = $("<div>").addClass("timer"); 
+var timerVal;
 var scoreBox = $("<div>").addClass("scoreBox");
 var scoreCurrent = $("<div>").addClass("scoreCurrent");
 var scoreCurrentVal = "scoreCurrent";
 var scoreSaved00 = $("<div>").addClass("scoreSaved");
 var scoreSavedVal00 = "Saved Score 00";
-var scoreSaved01 = $("<div>").addClass("scoreSaved");
+var scoreSaved01 = $("<div>").addClass("scoreSaved01");
 var scoreSavedVal01 = "Saved Score 00";
-var scoreSaved02 = $("<div>").addClass("scoreSaved");
+var scoreSaved02 = $("<div>").addClass("scoreSaved02");
 var scoreSavedVal02 = "Saved Score 00";
 
 var quiz = {
@@ -116,19 +117,6 @@ var quiz = {
     });
 
 
-/*function loadQuestion(){
-    var = questionBank[questionCurrent];
-
-};
-
-
-var questionTextValue = questionBank.question;
-var questionResponseVal00 = questionBank.response00;
-var questionResponseVal01 = questionBank.response01;
-var questionResponseVal02 = questionBank.response02;
-var questionResponseVal03 = questionBank.response03;
-*/
-
 
 containerEl.append(containerHeader);
 containerEl.append(containerBody);
@@ -143,15 +131,17 @@ questionBoxEl.append(questionResponse03);
 questionBoxEl.append(lineBreak00);
 
 var dataIndex = 0;
-var score = 0; // current score
+var score = 0; 
+var count = 30;
+var endTime = 0;
 
 function loadQuestion(){
     if (dataIndex < 6){
     questionTextBox.text(quiz.data[dataIndex].question);
     questionResponse00.text(quiz.data[dataIndex].responses[0]).attr('id', "correct");
-    questionResponse01.text(quiz.data[dataIndex].responses[1]);
-    questionResponse03.text(quiz.data[dataIndex].responses[2]);
-    questionResponse02.text(quiz.data[dataIndex].responses[3]);
+    questionResponse01.text(quiz.data[dataIndex].responses[1]).attr('id', "incorrect");
+    questionResponse03.text(quiz.data[dataIndex].responses[2]).attr('id', "incorrect");
+    questionResponse02.text(quiz.data[dataIndex].responses[3]).attr('id', "incorrect");
     }
     else{
         questionTextBox.css({"display": "none"});
@@ -169,20 +159,34 @@ loadQuestion();
 
 
 
-var count = 30;
-var endTime = 0;
 
 
 containerBody.append(sideBox);
 sideBox.append(timeBox);
+timeBox.text("Time Remaining")
 timeBox.append(timer);
-timer.text(count);
+timer.text(timerVal);
 
+timerVal = setInterval(loadTimer,1000);
 
-function loadCounter(){
+function loadTimer(){
         if (count >= endTime){
-        timer.text(count)
+        timer.text(count);    
         count--
+        
+    }
+    if (count == endTime){
+        questionTextBox.css({"display": "none"});
+        questionResponse00.css({"display": "none"});        
+        questionResponse01.css({"display": "none"});
+        questionResponse02.css({"display": "none"});
+        questionResponse03.css({"display": "none"});
+        lineBreak00.css({"display": "none"});
+        questionBoxEl.append(resultBox);
+        
+        
+
+        
     }
 };
 
@@ -206,6 +210,8 @@ scoreSaved01.text(scoreSavedVal01);
 scoreBox.append(scoreSaved02);
 scoreSaved02.text(scoreSavedVal02);
 
+
+
 $(".questionResponse").on("click", function (){
 
     dataIndex++;
@@ -213,11 +219,27 @@ $(".questionResponse").on("click", function (){
 
 });
 
+
+$("#incorrect").on("click", function (){
+
+    if (count >= 15){
+       count = 10; 
+    } else{
+        count = 5
+    }
+    
+    $(".timer").style.color = "#B91428";
+
+});
+
 $("#correct").on("click", function (){
+
+    
     score++;
     console.log(score);
     scoreCurrent.text(score +" / 6");
     resultBox.text(score +" / 6")
+    count = 30;
     var p = $("<p>")
     if (score > 3){
         resultBox.append(p);
@@ -226,6 +248,7 @@ $("#correct").on("click", function (){
         saveBtn.text("Save your score")
 
     } else{
+
         resultBox.append(p);
         p.text("Better Luck Next Time")
         resultBox.append(retryBtn);
@@ -237,15 +260,22 @@ $("#correct").on("click", function (){
     saveBtn.on("click", function (){
         userName = prompt("What are your initials?")
         localStorage.setItem("Saved_Score", (userName + ": " + score + " / 6"));
-        window.location.reload();
+        $(".scoreSaved").text(userName + ": " + score + " / 6");
+        count = 0;
+        scoreSavedVal02 = (localStorage.getItem("Saved_Score03"));
+        localStorage.setItem("Saved_Score04", (scoreSavedVal02));
+        scoreSavedVal01 = (localStorage.getItem("Saved_Score02"));
+        localStorage.setItem("Saved_Score03", (scoreSavedVal01));
+
+
+
+        
     });
 
-    
-    
-    
-
-
 });
+
+
+
 
 resultBox.text(score +" / 6")
 
